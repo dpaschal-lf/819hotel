e0010b299f91e4a7c8ed89fc5808f016a99f73d5
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-09-12 16:41:31 -0700

    fixed bug in room id main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 var mgmgrande = new Hotel();
 
-for( var roomID=100; roomID < 104; roomId++){
+for( var roomID=100; roomID < 104; roomID++){
     mgmgrande.addRoom( roomID );
 }
 mgmgrande.addCustomer('Uzair');
