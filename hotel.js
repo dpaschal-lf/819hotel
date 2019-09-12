06b1ea8c5ec6c682775b79fe4b08f5ffff97804d
Dane Maison <danemaison@gmail.com>
2019-09-12 16:47:49 -0700

    customers are automatically added to rooms, added roomService and checkout methods

--- hotel.js
+++ hotel.js
@@ -1,13 +1,26 @@
-
 class Hotel{
     constructor(){
-      this.rooms = []
-      this.customers = []
+      this.roomService = this.roomService.bind(this);
+      this.checkout = this.checkout.bind(this);
+      this.rooms = [];
+      this.customers = [];
     }
     addCustomer(name){
-      this.customers.push(new Customer(name))
+      var customer = new Customer(name, this.roomService, this.checkout);
+      this.customers.push(customer);
+      for(var i = 0; i < this.rooms.length; i++){
+        if(this.rooms[i].isVacant()){
+          this.rooms[i].addCustomer(customer)
+        }
+      }
     }
     addRoom(roomId){
-      this.rooms.push(new Room(roomId))
+      this.rooms.push(new Room(roomId));
+    }
+    roomService(){
+      console.log('customer ordered room service');
+    }
+    checkout(){
+      console.log('customer is checking out');
     }
 }
