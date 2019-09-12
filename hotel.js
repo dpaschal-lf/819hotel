505cb5bc0be2a18716d66ff699e2fd9122913ab4
Dane Maison <danemaison@gmail.com>
2019-09-12 16:38:02 -0700

    created structure for hotel

--- hotel.js
+++ hotel.js
@@ -1,4 +1,13 @@
 
 class Hotel{
-    
+    constructor(){
+      this.rooms = []
+      this.customers = []
+    }
+    addCustomer(name){
+      this.customers.push(new Customer(name))
+    }
+    addRoom(roomId){
+      this.rooms.push(new Room(roomId))
+    }
 }
