5786c5f44d4f1082e4abaf6e0deeb7f6450c376d
Mitchell O'Hair <mitchellohair@gmail.com>
2019-09-12 17:09:09 -0700

    Added methods to handle room service and a property & method to decide if the room is clean

--- room.js
+++ room.js
@@ -3,14 +3,27 @@ class Room {
     this.id = id || null;
     this.customer = null;
     this.vacant = true;
+    this.clean = true;
   }
 
   isVacant () {
     return this.vacant;
   }
 
+  isClean () {
+    return this.clean;
+  }
+
   addCustomer (customer) {
     this.customer = customer;
+    this.customer.checkIntoRoom(this);
     this.vacant = false;
+  }
+
+  deliverRoomService (item) {
+    if (this.customer) {
+      this.customer.receiveRoomService(item);
+      this.clean = false;
+    }
   }
 }
