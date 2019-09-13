e0130c4de45e19f0667a9c30e42a03e29c0b2ff3
dpaschal-lf <daniel.paschal@learningfuze.com>
2019-09-12 17:18:18 -0700

    added room service and checkin code

--- customer.js
+++ customer.js
@@ -1,10 +1,33 @@
 
 class Customer{
     constructor( name,  roomServiceCallback, checkOutCallback ){
+        this.orderRoomService = this.orderRoomService.bind( this );
+        this.currentRoom = null;
         this.callbacks = {
             roomservice: roomServiceCallback,
             checkout: checkOutCallback
         }
         this.name = name;
+        this.roomServiceTimer = null;
+        this.processActions();
+    }
+    processActions(){
+        var randomDurationToAskForService = Math.random() * 5000;
+        this.roomServiceTimer = setTimeout( this.orderRoomService, randomDurationToAskForService)
+    }
+    orderRoomService(){
+        var foodItems = ['hotdog','pizza','popcorn','champaign'];
+        var randomItem = foodItems[ Math.floor(Math.random()* foodItems.length)];
+        console.log(this.name+ ' ordering a ' + randomItem + ' from roomservice')
+        this.callbacks.roomservice(randomItem, this);
+    }
+    checkIntoRoom( roomObj ){
+        this.currentRoom = roomObj;
+    }
+    getCurrentRoom(){
+        return this.currentRoom;
+    }
+    receiveRoomService( item ){
+        console.log('I, '+ this.name + ' am eating a ' + item);
     }
 }
--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 
-
+var people = ['jan','uzair','sean','lana','james','brena','anjaleena','michael','christie','joseph','tarun','noel','christine','mitch','dane','heondo','adison','jordan','aaron']
 var mgmgrande = new Hotel();
 
 for( var roomID=100; roomID < 104; roomID++){
     mgmgrande.addRoom( roomID );
 }
-mgmgrande.addCustomer('Uzair');
-mgmgrande.addCustomer('Jan');
+
+function addRandomPersonToList(){
+    var randomPerson = people[ Math.floor( people.length * Math.random()) ];
+    var success = mgmgrande.addCustomer( randomPerson );
+    setTimeout( addRandomPersonToList , (Math.random()*20000)>>0) ;
+
+    if(success){
+        console.log('checked ' + randomPerson + ' into the hotel');
+    } else {
+        console.log('could not check ' + randomPerson + ' into the hotel, it was full');
+    }
+}
